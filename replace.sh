cb887a3bb28d483a145e4e77642b4b497a208772
David Phillips <david@sighup.nz>
2017-09-17 21:45:24 +1200

    Add script to macro-ify hardcoded values/paths

---
+++ replace.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -z $1 ] ; then
+	echo missing argument: file to read
+	exit 1
+fi
+
+file="$1"
+
+sedargs=""
+for var in \
+	BINDIR \
+	OPENRC_RUN \
+	; do
+	sedargs="${sedargs} -e s:\@${var}\@:${!var}:g "
+done
+
+cat "$file" | sed ${sedargs}
